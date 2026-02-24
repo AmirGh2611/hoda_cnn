08f5b9fa4e3e04567f056ea17cf652d76fae2946
amirhossein <amirabbasghorbani354@gmail.com>
2026-02-24 23:26:56 +0330

    class vision homework

---
+++ hoda_tensorboard.py
@@ -0,0 +1,12 @@
+from keras.losses import SparseCategoricalCrossentropy
+from keras.callbacks import TensorBoard
+from cnn_model import model
+from preprocess import x_train, y_train
+
+# tensorboard
+tensorboard = TensorBoard(log_dir="logs", histogram_freq=1)
+# loss and optimizer
+model.compile(optimizer="adam", loss=SparseCategoricalCrossentropy(from_logits=True), metrics=["accuracy"])
+# training
+model.fit(x_train, y_train, batch_size=64, epochs=25, validation_split=0.2, callbacks=[tensorboard])
+# to see the tensorboard results: tensorboard --logdir logs
