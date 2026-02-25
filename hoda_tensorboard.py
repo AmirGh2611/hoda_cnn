fbb89085009e9c417d16a1704c620d5937435cf7
amirhossein <amirabbasghorbani354@gmail.com>
2026-02-25 21:05:26 +0330

    .

--- hoda_tensorboard.py
+++ hoda_tensorboard.py
@@ -4,9 +4,9 @@ from cnn_model import model
 from preprocess import x_train, y_train
 
 # tensorboard
-tensorboard = TensorBoard(log_dir="logs", histogram_freq=1)
+tensorboard = TensorBoard(log_dir="logs")
 # loss and optimizer
 model.compile(optimizer="adam", loss=SparseCategoricalCrossentropy(from_logits=True), metrics=["accuracy"])
 # training
-model.fit(x_train, y_train, batch_size=64, epochs=25, validation_split=0.2, callbacks=[tensorboard])
+model.fit(x_train, y_train, batch_size=64, epochs=25, validation_split=0.2, callbacks=[tensorboard], verbose=0)
 # to see the tensorboard results: tensorboard --logdir logs
